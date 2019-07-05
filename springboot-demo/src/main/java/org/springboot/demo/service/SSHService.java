3afce2d803dcb90b429a48caf183c6b15157c9cd
shijia <1623299667@qq.com>
2019-07-05 10:49:27 +0800

    增加SSH操作

---
+++ springboot-demo/src/main/java/org/springboot/demo/service/SSHService.java
@@ -0,0 +1,120 @@
+package org.springboot.demo.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.SCPClient;
+import ch.ethz.ssh2.SCPInputStream;
+import ch.ethz.ssh2.SCPOutputStream;
+
+/**
+ * 远程操作linux系统
+ * @author shijia
+ *
+ */
+public class SSHService {
+	static String ip = "192.168.0.162";
+	static Integer port = 22;
+	static String name = "qinz";
+	static String password = "1";
+	static String remoteFile = "baishikele_600-r_0_2.jpg";
+	static String remoteTargetDirectory = "/share/data1/fastup/test";
+	static String newPath = "C:\\Users\\shijia\\Desktop\\";
+
+	public static void main(String[] args) {
+		File f = new File("test.txt");
+		uploadFile(ip, port, name, password, f, remoteTargetDirectory, null);
+	}
+
+	/**
+	 * 下载文件
+	 * 
+	 * @param ip
+	 * @param port
+	 * @param name
+	 * @param password
+	 * @param remoteFile
+	 * @param remoteTargetDirectory
+	 * @param newPath
+	 */
+	public static void download(String ip, Integer port, String name, String password, String remoteFile,
+			String remoteTargetDirectory, String newPath) {
+		Connection connection = new Connection(ip, port);
+
+		try {
+			connection.connect();
+			boolean isAuthenticated = connection.authenticateWithPassword(name, password);
+			if (isAuthenticated) {
+				SCPClient scpClient = connection.createSCPClient();
+				SCPInputStream sis = scpClient.get(remoteTargetDirectory + "/" + remoteFile);
+				File f = new File(newPath);
+				if (!f.exists()) {
+					f.mkdirs();
+				}
+				File newFile = new File(newPath + remoteFile);
+				FileOutputStream fos = new FileOutputStream(newFile);
+				byte[] b = new byte[4096];
+				int i;
+				while ((i = sis.read(b)) != -1) {
+					fos.write(b, 0, i);
+				}
+				fos.flush();
+				fos.close();
+				sis.close();
+			} else {
+				System.err.println("连接建立失败");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
+
+	/**
+	 * 上传文件
+	 * 
+	 * @param ip
+	 * @param port
+	 * @param name
+	 * @param password
+	 * @param f
+	 * @param remoteTargetDirectory
+	 * @param mode                  默认为null,源码会自动设置成0600，表示owner的读写权限，无执行权限
+	 */
+	public static void uploadFile(String ip, Integer port, String name, String password, File f,
+			String remoteTargetDirectory, String mode) {
+		Connection connection = new Connection(ip, port);
+
+		try {
+			connection.connect();
+			boolean isAuthenticated = connection.authenticateWithPassword(name, password);
+			if (!isAuthenticated) {
+				System.out.println("连接建立失败");
+				return;
+			}
+			SCPClient scpClient = new SCPClient(connection);
+			SCPOutputStream os = scpClient.put(f.getName(), f.length(), remoteTargetDirectory, mode);
+			byte[] b = new byte[4096];
+			FileInputStream fis = new FileInputStream(f);
+			int i;
+			while ((i = fis.read(b)) != -1) {
+				os.write(b, 0, i);
+			}
+			os.flush();
+			fis.close();
+			os.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
+}
