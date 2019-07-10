ffe932cced9e7b9de43c51022973d7770755e653
shijia <1623299667@qq.com>
2019-07-10 21:11:53 +0800

    增加http post请求实现

--- springboot-demo/src/main/java/org/springboot/demo/service/HttpService.java
+++ springboot-demo/src/main/java/org/springboot/demo/service/HttpService.java
@@ -1,5 +1,7 @@
 package org.springboot.demo.service;
 
+import java.util.Map.Entry;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -11,22 +13,24 @@ import com.alibaba.fastjson.JSONObject;
 
 public class HttpService {
 
-	public static String postJson(JSONObject json, String url) {
+	public static String postJson(String url,JSONObject body) {
+		JSONObject head = new JSONObject();
+		head.put("Content-Type", "application/json;charset=UTF-8");
+		return postJson(url, body, head);
+	}
+	
+	public static String postJson(String url,JSONObject body,JSONObject head) {
 		try {
 			CloseableHttpClient httpclient = HttpClients.createDefault();
-			System.out.println(json.toString());
-
+			System.out.println(body.toJSONString());
+			System.out.println(head.toJSONString());
 			HttpPost httpPost = new HttpPost(url);
-			httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
-
-			// 解决中文乱码问题
-			StringEntity stringEntity = new StringEntity(json.toString(), "UTF-8");
-			stringEntity.setContentEncoding("UTF-8");
-
+			for(Entry<String, Object> entry:head.entrySet()) {
+				httpPost.addHeader(entry.getKey(), (String) entry.getValue());
+			}
+			StringEntity stringEntity = new StringEntity(body.toString());
 			httpPost.setEntity(stringEntity);
-
 			System.out.println("Executing request " + httpPost.getRequestLine());
-
 			HttpEntity entity = httpclient.execute(httpPost).getEntity();
 			String responseBody = EntityUtils.toString(entity);
 			return responseBody;
@@ -40,7 +44,8 @@ public class HttpService {
 	public static void main(String[] args) {
 		JSONObject obj = new JSONObject();
 		obj.put("name", "张三");
-		String result = postJson(obj, "http://localhost:8080/hello/hello");
+		String url = "http://localhost:8080/hello/hello";
+		String result = postJson(url,obj);
 		System.out.println(result);
 	}
 }
