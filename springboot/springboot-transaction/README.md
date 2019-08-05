6ae09d067afa6bf2453f5ddbed02d3a14cfefb7e
shijia <1623299667@qq.com>
2019-08-05 10:52:29 +0800

    删除springboot事务模块，该模块代码移至interview代码查看

--- springboot/springboot-transaction/README.md
+++
@@ -1,4 +0,0 @@
-参考文章：
-
-SpringBoot 使用 @Transactional 注解配置事务：<https://blog.csdn.net/abysscarry/article/details/80189232>
-
--- springboot/springboot-transaction/src/main/java/org/springboot/transaction/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package org.springboot.transaction.mapper;
-
-import org.apache.ibatis.annotations.Param;
-import org.springboot.transaction.entity.User;
-
-public interface UserMapper {
-	void insertUser(@Param("user") User user);
-}
