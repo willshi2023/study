4a0344d2bbbbe40728bd7a878e3b0c26aa4fd8c6
shijia <1623299667@qq.com>
2019-08-10 20:03:34 +0800

    hystrix ribbon

---
+++ springcloud/service-hystrix/src/main/java/org/service/hystrix/service/HelloService.java
@@ -0,0 +1,27 @@
+package org.service.hystrix.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class HelloService {
+	@Autowired
+    RestTemplate restTemplate;
+
+	/**
+	 * 直接根据名字即可做负载均衡，自动找到ip和端口号
+	 * @param name
+	 * @return
+	 */
+	@HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+	
+	public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+}
