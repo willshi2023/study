d73236a410a4e05f52ec50969436b1c4daa80a8b
shijia <1623299667@qq.com>
2019-08-10 16:43:14 +0800

    ribbon负载均衡

--- springcloud/service-ribbon/src/main/java/org/service/ribbon/service/HelloService.java
+++ springcloud/service-ribbon/src/main/java/org/service/ribbon/service/HelloService.java
@@ -9,6 +9,11 @@ public class HelloService {
 	@Autowired
     RestTemplate restTemplate;
 
+	/**
+	 * 直接根据名字即可做负载均衡，自动找到ip和端口号
+	 * @param name
+	 * @return
+	 */
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
     }
