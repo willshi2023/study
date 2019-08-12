5817cf7710deb80b31c7faed4e433fe56a3af710
shijia <1623299667@qq.com>
2019-08-12 10:42:09 +0800

    路由网关zuul

---
+++ springcloud/service-zuul/src/main/java/org/service/zuul/ServiceZuulApplication.java
@@ -0,0 +1,34 @@
+package org.service.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * zuul有以下功能：
+
+Authentication
+Insights
+Stress Testing
+Canary Testing
+Dynamic Routing
+Service Migration
+Load Shedding
+Security
+Static Response handling
+Active/Active traffic management
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceZuulApplication.class, args );
+    }
+}
